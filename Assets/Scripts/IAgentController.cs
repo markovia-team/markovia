cbd257a05381541a0c5b56b70b97b4d902a38633
ebellver <ebellver@itba.edu.ar>
2021-09-18 20:43:44 -0300

    Added useless stuff :)

--- Assets/Scripts/IAgentController.cs
+++ Assets/Scripts/IAgentController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public interface IAgentController
 {
+   void moveTo(Vector3 to);
+   void runTo(Vector3 to);
+   void drink();
+   void eat();
+   void sleep();
+   // No seria void
+   void seeAround();
+
+
    // void Foo1();
    // void Foo2(); 
 }
