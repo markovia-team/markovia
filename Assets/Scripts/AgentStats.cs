bfa94a99514b0506dc86ddbce9f6fe3a9f56c5f1
gbeade <67883264+gbeade@users.noreply.github.com>
2021-09-18 13:06:45 -0300

    Create a Vector from a concat of enumerables

--- Assets/Scripts/AgentStats.cs
+++ Assets/Scripts/AgentStats.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
 using MathNet.Numerics.LinearAlgebra;
 using UnityEngine;
 
@@ -8,13 +10,14 @@ using UnityEngine;
 public class AgentStats
 {
     // Stats 
-    private Dictionary<Attribute, float> atts = new Dictionary<Attribute, float>();
-    private Dictionary<Need, float> needs = new Dictionary<Need, float>();
+    private SortedDictionary<Attribute, double> atts = new SortedDictionary<Attribute, double>();
+    private SortedDictionary<Need, double> needs = new SortedDictionary<Need, double>();
     
     // Factory class para matrices. 
     // Las matrices se crean a partir de métodos de la clase Matrix<T>.Build 
     // Ejemplo:  Matrix<T> foo( a1, a2, ..., an); 
-    MatrixBuilder<double> M = Matrix<double>.Build;
+    private MatrixBuilder<double> m = Matrix<double>.Build;
+    private VectorBuilder<double> v = Vector<double>.Build; 
     private Matrix<double> weights;
 
 
@@ -33,7 +36,9 @@ public class AgentStats
     // TODO: implement neural network 
     State nextState()
     {
-        return State.LookForWater;
+        v.DenseOfEnumerable(atts.Values.Concat(needs.Values));
+        return State.Stealth; 
     }
 
+    
 }
