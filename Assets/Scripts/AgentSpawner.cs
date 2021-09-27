f393bc9d3f06ec34aadc56357c4cdfa313dbd8e3
gbeade <67883264+gbeade@users.noreply.github.com>
2021-09-27 02:04:48 -0300

    AgentSpawner.cs

--- Assets/Scripts/AgentSpawner.cs
+++ Assets/Scripts/AgentSpawner.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class AgentSpawner : MonoBehaviour
 {
+    // Source for the serializable dictionary: 
+    // https://wiki.unity3d.com/index.php/File:SerializableDictionary.zip
     public SerializableDictionary<Species, GameObject> speciesPrefabs = new SerializableDictionary<Species, GameObject>();
+
+    
+    private Dictionary<Species, List<Vector3>> NonMovableAgentsPositions = new Dictionary<Species, List<Vector3>>();  
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach ( var s in speciesPrefabs )
+            if ( s.Value.GetComponent<NotMovableAgent>() != null )
+                NonMovableAgentsPositions.Add(s.Key, new List<Vector3>());
+        StartCoroutine(Populate()); 
     }
 
     // Update is called once per frame
@@ -17,4 +28,17 @@ public class AgentSpawner : MonoBehaviour
     {
         
     }
+
+    IEnumerator Populate()
+    {
+        while (true)
+        {
+            speciesPrefabs.TryGetValue(Species.Chicken, out var selectedPrefab); 
+            var reference = Instantiate(selectedPrefab, this.transform);
+           //  reference.GetComponent<Agent>().stats = SpeciesFactory.NewAgentStats(Species.Chicken); 
+            yield return new WaitForSeconds(5f); 
+        }
+    }
+    
+
 }
