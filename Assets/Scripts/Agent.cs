94b2e16e493617def223d10dc49ab9d831fb540d
ebellver <ebellver@itba.edu.ar>
2021-09-27 00:10:55 -0300

    Disgusting changes but hey it works :P

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -7,7 +7,7 @@ public abstract class Agent : MonoBehaviour, IAgentController
     private AgentStats stats;
     private State currentState = State.Wander;
     private State nextState = State.Wander;
-    private bool finished;
+    public bool finished = true;
     
     // Start is called before the first frame update
     public void Start()
@@ -20,13 +20,14 @@ public abstract class Agent : MonoBehaviour, IAgentController
     // En Update() van las cosas que deben ocurrir independientemente del control externo
     public void Update()
     {
+        /*
         if (!nextState.Equals(currentState) || finished) {
-            Debug.Log("Entro curr: " + currentState + "\t next: " + nextState);
             if (!finished)
                 StopCoroutine(SolveState());
             currentState = nextState;
             StartCoroutine(SolveState());
         }
+        */
     }
 
     // State nowState = stats.nextState();
@@ -44,16 +45,32 @@ public abstract class Agent : MonoBehaviour, IAgentController
     public IEnumerator GetNextState() {
         do {
             nextState = /*stats.NextState();*/ State.Wander;
-            Debug.Log(nextState);
-            // yield return new WaitForSecondsRealtime(1f);
-            yield return null;
+            yield return new WaitForSecondsRealtime(1f);
+            // yield return null;
         } while(true);
     }
 
+    /*
     public IEnumerator SolveState() {
         finished = false;
         currentState.SolveState(this, this.gameObject);
         finished = true;
+        Debug.Log("finished");
         yield return null;
+    }
+    */
+    
+    public IEnumerator SolveState()
+    {
+        while(true)
+        {
+            if (!nextState.Equals(currentState) || finished)
+            {
+                finished = false;
+                currentState.SolveState(this, this);
+                Debug.Log(finished);
+            }
+            yield return null;
+        }
     }
 }
