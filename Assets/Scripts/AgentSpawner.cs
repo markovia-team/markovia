37123f5055964ee338126ebb8292ac8db0fc5a98
gbeade <67883264+gbeade@users.noreply.github.com>
2021-09-27 01:21:08 -0300

    SerializableDictionary
    Para cargar prefabs en un diccionario desde el inspector

---
+++ Assets/Scripts/AgentSpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentSpawner : MonoBehaviour
+{
+    public SerializableDictionary<Species, GameObject> speciesPrefabs = new SerializableDictionary<Species, GameObject>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
