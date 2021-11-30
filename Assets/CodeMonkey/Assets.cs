7f2e4b41204739dd8dfe8ef65b9b786ea4476607
Santiago Lo Coco <slococo@itba.edu.ar>
2021-11-30 12:05:35 -0300

    Add line and bar chart

---
+++ Assets/CodeMonkey/Assets.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeMonkey {
+    public class Assets : MonoBehaviour {
+
+        // Internal instance reference
+        private static Assets _i; 
+
+        // Instance reference
+        public static Assets i {
+            get {
+                if (_i == null) _i = Instantiate(Resources.Load<Assets>("CodeMonkeyAssets")); 
+                return _i; 
+            }
+        }
+
+
+        // All references
+        
+        public Sprite s_White;
+        public Sprite s_Circle;
+
+        public Material m_White;
+
+    }
+
+}
---
+++ Assets/CodeMonkey/MonoBehaviours/CameraFollowSetup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeMonkey.MonoBehaviours {
+    public class CameraFollowSetup : MonoBehaviour {
+
+        [SerializeField] private CameraFollow cameraFollow = null;
+        [SerializeField] private Transform followTransform = null;
+        [SerializeField] private float zoom = 50f;
+
+        private void Start() {
+            if (followTransform == null) {
+                Debug.LogError("followTransform is null! Intended?");
+                cameraFollow.Setup(() => Vector3.zero, () => zoom, true, true);
+            } else {
+                cameraFollow.Setup(() => followTransform.position, () => zoom, true, true);
+            }
+        }
+    }
+
+}
