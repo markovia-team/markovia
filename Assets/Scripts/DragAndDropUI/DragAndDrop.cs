c745c00373efa4b5977081fc05cc23b72afd9cac
Santiago Lo Coco <slococo@itba.edu.ar>
2021-12-01 08:49:26 -0300

    Fix OnDrag mouse issue

--- Assets/Scripts/DragAndDropUI/DragAndDrop.cs
+++ Assets/Scripts/DragAndDropUI/DragAndDrop.cs
@@ -5,7 +5,6 @@ using UnityEngine.EventSystems;
 
 public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-
     [SerializeField] private Canvas canvas; 
     private RectTransform rectTransform;
     private Vector2 originPos;
@@ -19,17 +18,14 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("OnPointerDown");
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("OnBeginDrag");
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
         rectTransform.anchoredPosition = originPos; 
         
         Ray ray = Camera.main.ScreenPointToRay(eventData.position);
@@ -43,19 +39,17 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
             // Debug.DrawLine (ray.origin, hit.point);
             // //log hit area to the console
             // Debug.Log(hit.point);
-
         }    
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-       // Debug.Log("OnDrag");
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor; 
     } 
     
-    private static Rect RectTransformToScreenSpace(RectTransform transform)
-    {
-        Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
-        return new Rect((Vector2)transform.position - (size * 0.5f), size);
-    }
+    // private static Rect RectTransformToScreenSpace(RectTransform transform)
+    // {
+    //     Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
+    //     return new Rect((Vector2)transform.position - (size * 0.5f), size);
+    // }
 }
