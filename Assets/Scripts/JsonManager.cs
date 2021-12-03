f7bf7fc53d517b77042889e5c8fa226fcc5fbb8c
Santiago Lo Coco <slococo@itba.edu.ar>
2021-12-03 15:11:08 -0300

    Add load from and save to file (with file picker for Windows, MacOS and Linux

--- Assets/Scripts/JsonManager.cs
+++ Assets/Scripts/JsonManager.cs
@@ -4,21 +4,21 @@ using System.IO;
 public static class JsonManager {
     public static bool SaveToJson(string fileName, GameData data) {
         // No tira error el writeAll pero está bueno chequear que no sea read only de todas formas:
-        if (File.Exists(Application.dataPath + "/Scripts/" + fileName) && ((File.GetAttributes(Application.dataPath + "/Scripts/" + fileName) & FileAttributes.ReadOnly)) != 0)
+        if (File.Exists(fileName) && ((File.GetAttributes(fileName) & FileAttributes.ReadOnly)) != 0)
             return false;
         
         string textJson = JsonUtility.ToJson(data);
-        File.WriteAllText(Application.dataPath + "/Scripts/" + fileName, textJson);
+        File.WriteAllText(fileName, textJson);
         return true;
     }
 
     public static bool ReadFromJson(string fileName, out GameData dataBuff) {
-        if (!File.Exists(Application.dataPath + "/Scripts/" + fileName)) {
+        if (!File.Exists(fileName)) {
             dataBuff = null;
             return false;
         }
 
-        string textJson = File.ReadAllText(Application.dataPath + "/Scripts/" + fileName);
+        string textJson = File.ReadAllText(fileName);
         dataBuff = JsonUtility.FromJson<GameData>(textJson);
         return true;
     }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 using System.Text;
 using System.IO;
 using System.Linq;
+using SFB;
 // using UnityEngine.UIElements;
 using UnityEngine.UI;
 
@@ -13,11 +14,12 @@ public class MainMenu : MonoBehaviour
 {
     private Dictionary<Species, GameObject> speciesPrefabs = new Dictionary<Species, GameObject>();
     public GameObject button;
+    public GameObject popup;
 
     private void Awake() {
-        if (!File.Exists(Application.dataPath + "/Scripts/AgentSpawnerFile.json")) {
-            button.GetComponent<Button>().interactable = false;
-        }
+        // if (!File.Exists(Application.dataPath + "/Scripts/AgentSpawnerFile.json")) {
+        //     button.GetComponent<Button>().interactable = false;
+        // }
     }
 
     public void PlayGame() {
@@ -57,25 +59,38 @@ public class MainMenu : MonoBehaviour
     }
 
     public void StartFile() {
-        speciesPrefabs = new Dictionary<Species, GameObject>();
-        GameObject prefab;
-        GameData savedData;
-        if (!JsonManager.ReadFromJson("AgentSpawnerFile.json", out savedData)) {
+        var path = StandaloneFileBrowser.OpenFilePanel("Open File", "", "", false)[0];
+        if (string.Compare(path, string.Empty, StringComparison.Ordinal) == 0) {
+            popup.GetComponentInChildren<TMPro.TMP_Text>().text = "You must choose a json file";
+            showPopup();
             return;
         }
 
+        speciesPrefabs = new Dictionary<Species, GameObject>();
+        GameObject prefab;
+        GameData savedData;
+        try {
+            if (!JsonManager.ReadFromJson(path, out savedData)) {
+                return;
+            }
+        } catch(ArgumentException) {
+            popup.GetComponentInChildren<TMPro.TMP_Text>().text = "Invalid json";
+            showPopup();
+            return;
+        }
+        
         string[] agents = savedData.getAgentList().Split('\n');
 
-        foreach(string agent in agents) {
-            if(agent.CompareTo("Grass") == 0){
+        foreach (string agent in agents) {
+            if (agent.CompareTo("Grass") == 0) {
                 prefab = (GameObject) Resources.Load(agent, typeof(GameObject));
                 speciesPrefabs.Add(Species.Grass, prefab);
             }
-            else if(agent.CompareTo("Fox") == 0){
+            else if (agent.CompareTo("Fox") == 0) {
                 prefab = (GameObject) Resources.Load(agent, typeof(GameObject));
                 speciesPrefabs.Add(Species.Fox, prefab);
             }
-            else if(agent.CompareTo("Chicken") == 0){
+            else if (agent.CompareTo("Chicken") == 0) {
                 prefab = (GameObject) Resources.Load(agent, typeof(GameObject));
                 speciesPrefabs.Add(Species.Chicken, prefab);
             }
@@ -83,5 +98,11 @@ public class MainMenu : MonoBehaviour
         PlayGame();
     }
         
+    public void hidePopup() {
+        popup.SetActive(false);
+    }
 
+    public void showPopup() {
+        popup.SetActive(true);
+    }
 }
