6c29838759d78301f3ba995ffbf47954d3f93681
salvaCasta <salCas276@gmail.com>
2021-12-03 15:12:28 -0300

    Agrego texto que se muestra cuando se clickea un agente

--- Assets/Scripts/AgentDataPrinter.cs
+++ Assets/Scripts/AgentDataPrinter.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 
 public class AgentDataPrinter : MonoBehaviour {
+    private int timer = -1;
 
+    void Start(){
+        if(gameObject.GetComponent<Text>() != null)
+            gameObject.GetComponent<Text>().text = string.Empty;
+    }
     void Update(){
         if(Input.GetMouseButtonDown(0)){
             RaycastHit hit;
@@ -13,7 +19,23 @@ public class AgentDataPrinter : MonoBehaviour {
             if(Physics.Raycast(ray, out hit)){
                 var selection = hit.transform;
                 if(selection.gameObject.CompareTag("Printable") && selection!= null){
-                    Debug.Log(selection.gameObject.GetComponent<Agent>().ToString());
+                    if(gameObject.GetComponent<Text>() != null){
+                        gameObject.GetComponent<Text>().text = selection.gameObject.GetComponent<Agent>().ToString();
+                        timer = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    void FixedUpdate(){
+        if(timer != -1){
+            timer++;
+            //Lo muestra durante 5 segs
+            if(timer >= 250){
+                if(gameObject.GetComponent<Text>() != null){
+                    gameObject.GetComponent<Text>().text = string.Empty;
+                    timer = -1;
                 }
             }
         }
