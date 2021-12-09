e74d1fc3b7b73d4bbc819114d32ac20ba60b7f99
Santiago Lo Coco <slococo@itba.edu.ar>
2021-12-09 11:29:56 -0300

    Use AgentSpawner

--- Assets/Scripts/UI/AllSpeciesHolder.cs
+++ Assets/Scripts/UI/AllSpeciesHolder.cs
@@ -3,15 +3,17 @@ using UnityEngine.UI;
 
 public class AllSpeciesHolder : MonoBehaviour {
     [SerializeField] private GameObject speciesHolderPrefab;
-    public SerializableDictionary<Species, Texture> speciesIcon = new SerializableDictionary<Species, Texture>();
-    public Canvas canvas;
+    [SerializeField] private SerializableDictionary<Species, Texture> speciesIcon = new SerializableDictionary<Species, Texture>();
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private WorldController controller;
 
     void Awake() {
         var i = 0;
         foreach (var x in speciesIcon) {
-            var go = Instantiate(speciesHolderPrefab, new Vector2(120 + (i * 120) * canvas.scaleFactor, 100), speciesHolderPrefab.transform.rotation, transform).transform.GetChild(0);
+            var go = Instantiate(speciesHolderPrefab, new Vector2(120 + i * 120 * canvas.scaleFactor, 100), speciesHolderPrefab.transform.rotation, transform).transform.GetChild(0);
             var dad = go.GetComponent<DragAndDrop>();
-            dad.Canvas = canvas;
+            dad.agentSpawner = controller.GetComponent<AgentSpawner>();
+            dad.canvas = canvas;
             dad.species = x.Key;
             dad.GetComponent<RawImage>().texture = x.Value;
             i++;
