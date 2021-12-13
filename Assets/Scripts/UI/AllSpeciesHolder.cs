65ea866329728a3c11a80e55e6a5a8684b6d2a4a
gbeade <67883264+gbeade@users.noreply.github.com>
2021-12-13 13:08:50 -0300

    UI changes

--- Assets/Scripts/UI/AllSpeciesHolder.cs
+++ Assets/Scripts/UI/AllSpeciesHolder.cs
@@ -10,7 +10,7 @@ public class AllSpeciesHolder : MonoBehaviour {
     void Awake() {
         var i = 0;
         foreach (var x in speciesIcon) {
-            var go = Instantiate(speciesHolderPrefab, new Vector2(120 + i * 120 * canvas.scaleFactor, 60),
+            var go = Instantiate(speciesHolderPrefab, transform.position+new Vector3( (i % 2) * 120 * canvas.scaleFactor-20, (i/2)*120*canvas.scaleFactor-50),
                 speciesHolderPrefab.transform.rotation, transform).transform.GetChild(0);
             var dad = go.GetComponent<DragAndDrop>();
             dad.agentSpawner = controller.GetComponent<AgentSpawner>();
